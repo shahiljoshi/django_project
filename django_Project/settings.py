d101724c13fe0b2ffe5faa7d57f1f48ee43badea
shahiljoshi <shahil.inexture@gmail.com>
2021-05-10 17:00:57 +0530

    procfile change

--- django_Project/settings.py
+++ django_Project/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 import django.core.mail.backends.base
 
+
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
@@ -24,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+#SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-38z_icthtw-k1lnxqdp_o--n=njvtjq&h4ye57244_9jr=7l3*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
