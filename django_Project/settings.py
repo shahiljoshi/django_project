59ff99ab154ffa446d4e34808e0d156a1f750cb7
shahiljoshi <shahil.inexture@gmail.com>
2021-05-10 15:07:09 +0530

    fix:static dir change

--- django_Project/settings.py
+++ django_Project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','mydjangoblog-project.herokuapp.com']
+ALLOWED_HOSTS = ['mydjangoblog-project.herokuapp.com']
 
 
 # Application definition
@@ -158,11 +158,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 SITE_ID = 2
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+STATICFILES_DIRS =[
+        os.path.join(BASE_DIR, 'static'),
+    ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
